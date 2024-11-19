10f262a4ea7e46e7e698c5429cd7926ff11a8886
Prazen Manandhar <prazenmndhar@gmail.com>
2024-11-19 10:14:41 +0545

    Update frontend and backend Added

---
+++ backend/data/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: "1",
+    name: "Airpods Wireless Bluetooth",
+    image: "/images/airpods.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: 24999,
+    countInStock: 10,
+    rating: 3.9,
+    numReviews: 7,
+  },
+  {
+    _id: "2",
+    name: "iPhone 13 Pro 256GB Memory",
+    image: "/images/phone.jpg",
+    description:
+      "Introducing the iPhone 13 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
+    brand: "Apple",
+    category: "Electronics",
+    price: 191990,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Cannon EOS 80D DSLR Camera",
+    image: "/images/camera.jpg",
+    description:
+      "Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design",
+    brand: "Cannon",
+    category: "Electronics",
+    price: 99999,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Sony Playstation 5",
+    image: "/images/playstation.jpg",
+    description:
+      "The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music",
+    brand: "Sony",
+    category: "Electronics",
+    price: 90000,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "Logitech G-102 Gaming Mouse",
+    image: "/images/mouse.jpg",
+    description:
+      "Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience",
+    brand: "Logitech",
+    category: "Electronics",
+    price: 499,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: "6",
+    name: "Ultima Marvel Speaker",
+    image: "/images/Ultima.jpg",
+    description:
+      "The Ultima Marvel Speaker combines an ergonomic design with durable plastic construction and dynamic RGB LED lights.",
+    brand: "Ultima",
+    category: "Electronics",
+    price: 2699,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 12,
+  },
+];
+
+export default products;
